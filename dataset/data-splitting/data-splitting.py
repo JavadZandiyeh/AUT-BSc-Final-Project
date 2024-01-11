ee74a773e0d424ff91c0b2b29df970fe216800ad
javad <zandiyeh1379@gmail.com>
2024-01-11 15:40:28 +0330

    Fix train, test, val data

--- dataset/data-splitting/data-splitting.py
+++ dataset/data-splitting/data-splitting.py
@@ -1,53 +1,50 @@
 import configparser
 import csv
 import sys
+from enum import Enum
 
 import numpy as np
 import torch
 from torch_geometric.data import Dataset, Data
 
-config = configparser.ConfigParser()
-config.read('config.ini')
 
-
-def train_test_validation_division(matrix):
-    """ Convert an adjacency matrix to train, test, and validation
+def train_val_test_mask(matrix):
+    """ Convert an adjacency matrix to train, val, and test masks
         Args:
-            matrix: adjacency matrix
+            matrix: symmetric adjacency matrix with shape (n * n)
 
         Returns:
-            three matrices contains named: 1. train (80%), 2. test (10%), 3. validation (10%)
+            three matrices contains named: 1. train_mask (80%), 2. val_mask (10%), 3. test_mask (10%)
             these matrices divide our matrix into three parts, which are used for the
             learning procedure.
             example:
-            train, test, validation = train_test_validation(matrix)
+            train_mask, val_mask, test_mask = train_val_test_mask(matrix)
     """
+    matrix_triu = np.triu(matrix, k=0)  # upper triangular matrix, with the diagonal (k=0)
 
-    num_nonzero = np.count_nonzero(matrix)
+    num_nonzero = np.count_nonzero(matrix_triu)
     rand_arr = np.zeros(num_nonzero)
     index_1, index_2 = int(0.8 * num_nonzero), int(0.9 * num_nonzero)
-    rand_arr[:index_1], rand_arr[index_1:index_2], rand_arr[index_2:] = 1, 2, 3  # train, test, validation
+    rand_arr[:index_1], rand_arr[index_1:index_2], rand_arr[index_2:] = 1, 2, 3  # train, val, test
     np.random.shuffle(rand_arr)
 
     rand_index = 0
-    ui_adj = np.copy(matrix)  # divide user-item adjacency matrix to 3 parts of (80%, 10%, 10%)
-    row, col = matrix.shape
+    row, col = matrix_triu.shape
     for i in range(row):
         for j in range(col):
-            if ui_adj[i][j] != 0:
-                ui_adj[i][j] = rand_arr[rand_index]
+            if matrix_triu[i][j] != 0:
+                matrix_triu[i][j] = rand_arr[rand_index]
                 rand_index += 1
 
-    def get_matrix_values(matrix_type):
-        mask_matrix = np.zeros_like(matrix)
-        mask_matrix[ui_adj == matrix_type] = 1
-        return mask_matrix * matrix
+    def get_mask(mask_type):
+        mask_triu = np.zeros_like(matrix_triu)
+        mask_triu[matrix_triu == mask_type] = 1  # with diagonal
+        mask_tril = np.triu(mask_triu, k=1).T  # without diagonal
+        return mask_triu + mask_tril
 
-    train = get_matrix_values(1)  # matrix_type of 1 is the train matrix
-    test = get_matrix_values(2)  # matrix_type of 2 is the test matrix
-    validation = get_matrix_values(3)  # matrix_type of 3 is the validation matrix
+    train, val, test = get_mask(1), get_mask(2), get_mask(3)
 
-    return train, test, validation
+    return train, val, test
 
 
 def matrix_sparsify(matrix, degree_cutoff):
@@ -116,14 +113,23 @@ def matrix_sparsify2(matrix, degree_cutoff):
     return matrix * mask
 
 
+class DatasetType(Enum):
+    imdb_1m = 'imdb-1m'
+    imdb_25m = 'imdb-25m'
+
+
 class IMDbDataset(Dataset):
     def __init__(self, root, transform=None, pre_transform=None, pre_filter=None):
-        # Initialize matrices
-        self.user_names, self.user_features, self.item_names = None, None, None
-        self.item_features, self.item_interactions_diff, self.ui_weights = None, None, None
-
         # Dimension of matrices (because the .dat file doesn't store dimensions of matrices)
-        self.num_items, self.num_users, self.num_item_features = 3848, 6040, 31
+        self.num_users = 6040
+        if dataset_type == DatasetType.imdb_1m.value:  # imdb-1m
+            self.num_items, self.num_item_features = 3848, 31
+        else:  # imdb-25m
+            self.num_items, self.num_item_features = 62390, 35
+
+        # Initialize matrices
+        # self.item_names, self.item_interactions, self.ii_weights, self.user_names = None, None, None, None
+        self.user_features, self.item_features, self.item_interactions_diff, self.iu_weights = None, None, None, None
 
         # Connections between items with a weight less than this threshold are ignored
         self.item_interactions_diff_threshold = config.getfloat('sparsify', 'edge_weight')
@@ -136,105 +142,105 @@ class IMDbDataset(Dataset):
     @property
     def raw_file_names(self):
         """ If these files exist in raw_dir, the download is not triggered """
-        return ['user_names.csv', 'user_features.dat', 'item_names.csv', 'item_features.dat',
-                'item_interactions_diff.dat', 'ui_weights.dat']  # The order matters in the process() function.
+        return ['item_names.csv', 'item_interactions.dat', 'item_interactions_diff.dat', 'item_features.dat',
+                'ii_weights.dat', 'user_names.csv', 'user_features.dat', 'iu_weights.dat']
 
     @property
     def processed_file_names(self):
         """ If these files are found in raw_dir, processing is skipped """
-        return ['data_ii.pt', 'data_ui_train.pt', 'data_ui_test.pt', 'data_ui_validation.pt']
+        return ['data.pt']
 
     def download(self):
         """ Download raw files if they do not exist in our raw directory """
         pass  # For this dataset, all raw files are already available, and there is no need to download anything.
 
     def process(self):
-        # Fetch matrices
-        self.raw_file_content()
+        # fetch required matrices to create our graphs
+        self.fetch_matrices()
 
-        """ item-item dataset """
-        # part 1: trim edges below the weight threshold
-        # self.item_interactions_diff[self.item_interactions_diff < self.item_interactions_diff_threshold] = 0
+        """ x: [(num_users + num_items), num_item_features] """
+        x = np.vstack((self.user_features, self.item_features))
 
-        # part 2: make the matrix sparse
-        sparse_item_interaction_diff = matrix_sparsify(self.item_interactions_diff, self.max_item_degree_threshold)
+        """ y: [(num_users + num_items), (num_users, num_items)] """
+        y_uu = np.zeros((self.num_users, self.num_users))
+        y_ii = np.zeros((self.num_items, self.num_items))
 
-        ii_edge_index = np.asarray(np.nonzero(sparse_item_interaction_diff))
-        ii_edge_attr = sparse_item_interaction_diff[ii_edge_index[0], ii_edge_index[1]]
+        y = np.vstack((np.hstack((y_uu, self.iu_weights.T)), np.hstack((self.iu_weights, y_ii))))
 
-        data_ii = Data(
-            x=torch.tensor(self.item_features, dtype=torch.float),  # node features
-            edge_index=torch.tensor(ii_edge_index, dtype=torch.long),  # edges between items
-            edge_attr=torch.tensor(ii_edge_attr, dtype=torch.float)  # weights related to edges
+        """ adj: [(num_users + num_items), (num_users, num_items)] """
+        adj_uu = np.zeros((self.num_users, self.num_users))
+        adj_iu = np.where(self.iu_weights != 0, 1, 0)
+        adj_ii = matrix_sparsify(self.item_interactions_diff, self.max_item_degree_threshold)
+
+        adj = np.vstack((np.hstack((adj_uu, adj_iu.T)), np.hstack((adj_iu, adj_ii))))
+
+        """ mask_ii, mask_uiu: [(num_users + num_items), (num_users, num_items)] """
+        mask_ii = np.zeros_like(adj)
+        mask_ii[self.num_users:, self.num_users:] = 1
+
+        mask_uiu = np.zeros_like(adj)
+        mask_uiu[:self.num_users, self.num_users:] = 1
+        mask_uiu[self.num_users:, :self.num_users] = 1
+
+        """ mask_train, mask_val, mask_test: [(num_users + num_items), (num_users, num_items)] """
+        mask_train_ii, mask_val_ii, mask_test_ii = train_val_test_mask(mask_ii * adj)
+        mask_train_uiu, mask_val_uiu, mask_test_uiu = train_val_test_mask(mask_uiu * adj)
+
+        """ save data in the pyg dataset format"""
+        edge_index = np.asarray(np.nonzero(adj))
+
+        edge_y = y[edge_index[0], edge_index[1]]
+
+        edge_attr = adj[edge_index[0], edge_index[1]]
+
+        edge_mask_ii = np.where(mask_ii * adj != 0, True, False)
+        edge_mask_ii = edge_mask_ii[edge_index[0], edge_index[1]]
+
+        edge_mask_uiu = np.where(mask_uiu * adj != 0, True, False)
+        edge_mask_uiu = edge_mask_uiu[edge_index[0], edge_index[1]]
+
+        edge_mask_train = np.where((mask_train_ii + mask_train_uiu) * adj != 0, True, False)
+        edge_mask_train = edge_mask_train[edge_index[0], edge_index[1]]
+
+        edge_mask_val = np.where((mask_val_ii + mask_val_uiu) * adj != 0, True, False)
+        edge_mask_val = edge_mask_val[edge_index[0], edge_index[1]]
+
+        edge_mask_test = np.where((mask_test_ii + mask_test_uiu) * adj != 0, True, False)
+        edge_mask_test = edge_mask_test[edge_index[0], edge_index[1]]
+
+        data = Data(
+            x=torch.tensor(x, dtype=torch.float),
+            y=torch.tensor(edge_y, dtype=torch.float),
+            edge_index=torch.tensor(edge_index, dtype=torch.long),
+            edge_attr=torch.tensor(edge_attr, dtype=torch.float),
+            edge_mask_uiu=torch.tensor(edge_mask_uiu, dtype=torch.bool),
+            edge_mask_ii=torch.tensor(edge_mask_ii, dtype=torch.bool),
+            edge_mask_train=torch.tensor(edge_mask_train, dtype=torch.bool),
+            edge_mask_val=torch.tensor(edge_mask_val, dtype=torch.bool),
+            edge_mask_test=torch.tensor(edge_mask_test, dtype=torch.bool)
         )
-        # Example: data_ii = Data(x=[3848, 31], edge_index=[2, 501336], edge_attr=[501336])
 
-        torch.save(data_ii, f'{self.processed_dir}/data_ii.pt')
+        torch.save(data, f'{self.processed_dir}/data.pt')
 
-        """ user-item train, test, and validation datasets """
-        # part 1
-        ui_node_features = np.vstack((self.item_features, self.user_features))
-
-        # part 2
-        ui_weights_train, ui_weights_test, ui_weights_validation = train_test_validation_division(self.ui_weights)
-
-        def get_ui_edge_weight_completed(ui_weights):
-            ui_edge_index_part1 = np.vstack((ui_weights, np.zeros((self.num_users, self.num_users))))
-            ui_edge_index_part2 = np.vstack((np.zeros((self.num_items, self.num_items)), ui_weights.T))
-            ui_edge_index = np.hstack((ui_edge_index_part1, ui_edge_index_part2))
-            return ui_edge_index
-
-        ui_edge_weights_train = get_ui_edge_weight_completed(ui_weights_train)
-        ui_edge_weights_test = get_ui_edge_weight_completed(ui_weights_test)
-        ui_edge_weights_validation = get_ui_edge_weight_completed(ui_weights_validation)
-
-        # part 3
-        ui_edge_index_train = np.asarray(np.where(ui_edge_weights_train > 0))
-        ui_edge_index_test = np.asarray(np.where(ui_edge_weights_test > 0))
-        ui_edge_index_validation = np.asarray(np.where(ui_edge_weights_validation > 0))
-
-        # part 4
-        ui_edge_attr_train = ui_edge_weights_train[ui_edge_index_train[0], ui_edge_index_train[1]]
-        ui_edge_attr_test = ui_edge_weights_test[ui_edge_index_test[0], ui_edge_index_test[1]]
-        ui_edge_attr_validation = ui_edge_weights_validation[ui_edge_index_validation[0], ui_edge_index_validation[1]]
-
-        # part 5
-        def get_data_ui(ui_edge_index, ui_edge_attr):
-            return Data(
-                x=torch.tensor(ui_node_features, dtype=torch.float),
-                edge_index=torch.tensor(ui_edge_index, dtype=torch.long),
-                edge_attr=torch.tensor(ui_edge_attr, dtype=torch.float)
-            )
-
-        data_ui_train = get_data_ui(ui_edge_index_train, ui_edge_attr_train)
-        data_ui_test = get_data_ui(ui_edge_index_test, ui_edge_attr_test)
-        data_ui_validation = get_data_ui(ui_edge_index_validation, ui_edge_attr_validation)
-
-        torch.save(data_ui_train, f'{self.processed_dir}/data_ui_train.pt')
-        torch.save(data_ui_test, f'{self.processed_dir}/data_ui_test.pt')
-        torch.save(data_ui_validation, f'{self.processed_dir}/data_ui_validation.pt')
-
-        # Examples:
-        # data_ui_train = Data(x=[9888, 31], edge_index=[2, 1594986], edge_attr=[1594986])
-        # data_ui_test = Data(x=[9888, 31], edge_index=[2, 199374], edge_attr=[199374])
-        # data_ui_validation = Data(x=[9888, 31], edge_index=[2, 199374], edge_attr=[199374])
-
-    def raw_file_content(self):
+    def fetch_matrices(self):
         """ Fetch data and set our matrices """
-        with open(self.raw_paths[0], 'r', newline='') as csv_file:
-            csv_reader = csv.reader(csv_file)
-            self.user_names = [user_name[0] for user_name in csv_reader]
+        # with open(self.raw_paths[0], 'r', newline='') as csv_file:
+        #     csv_reader = csv.reader(csv_file)
+        #     self.item_names = [item_name[0] for item_name in csv_reader]
 
-        self.user_features = np.memmap(
-            self.raw_paths[1],
+        # self.item_interactions = np.memmap(
+        #     self.raw_paths[1],
+        #     dtype=np.float32,
+        #     mode='r',
+        #     shape=(self.num_items, 1)
+        # )
+
+        self.item_interactions_diff = np.memmap(
+            self.raw_paths[2],
             dtype=np.float32,
             mode='r',
-            shape=(self.num_users, self.num_item_features)
+            shape=(self.num_items, self.num_items)
         )
-
-        with open(self.raw_paths[2], 'r', newline='') as csv_file:
-            csv_reader = csv.reader(csv_file)
-            self.item_names = [item_name[0] for item_name in csv_reader]
 
         self.item_features = np.memmap(
             self.raw_paths[3],
@@ -243,17 +249,26 @@ class IMDbDataset(Dataset):
             shape=(self.num_items, self.num_item_features)
         )
 
-        # item-item weights
-        self.item_interactions_diff = np.memmap(
-            self.raw_paths[4],
+        # self.ii_weights = np.memmap(
+        #     self.raw_paths[4],
+        #     dtype=np.float32,
+        #     mode='r',
+        #     shape=(self.num_items, self.num_items)
+        # )
+
+        # with open(self.raw_paths[5], 'r', newline='') as csv_file:
+        #     csv_reader = csv.reader(csv_file)
+        #     self.user_names = [user_name[0] for user_name in csv_reader]
+
+        self.user_features = np.memmap(
+            self.raw_paths[6],
             dtype=np.float32,
             mode='r',
-            shape=(self.num_items, self.num_items)
+            shape=(self.num_users, self.num_item_features)
         )
 
-        # user-item weights
-        self.ui_weights = np.memmap(
-            self.raw_paths[5],
+        self.iu_weights = np.memmap(
+            self.raw_paths[7],
             dtype=np.float32,
             mode='r',
             shape=(self.num_items, self.num_users)
@@ -268,8 +283,11 @@ class IMDbDataset(Dataset):
 
 
 if __name__ == '__main__':
-    dataset = IMDbDataset(root=sys.argv[1])
-    print('data_ii: ', dataset.get('data_ii'))
-    print('data_ui_train: ', dataset.get('data_ui_train'))
-    print('data_ui_test: ', dataset.get('data_ui_test'))
-    print('data_ui_validation: ', dataset.get('data_ui_validation'))
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    dataset_type = DatasetType.imdb_1m.value
+
+    dataset = IMDbDataset(root=dataset_type)
+
+    print('data: ', dataset.get('data'))
--- dataset/data-splitting/run.sh
+++
@@ -1,1 +0,0 @@
-python3 data-splitting.py 'imdb-1m'
